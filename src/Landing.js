66bd53f713e0381ef445a435704a257c2010b585
Nasir Khalid <75773436+NasirKhalidQ@users.noreply.github.com>
2021-07-31 12:27:57 +0500

    change logo

--- src/Landing.js
+++ src/Landing.js
@@ -8,11 +8,11 @@ function Landing() {
             <div className="flex-1 flex-col items-center lg:items-start">
                 <h2 className="text-accusoft-yellow text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6">It consulting services</h2>
                 <p className="text-accusoft-white text-lg text-center lg:text-left mb-6">
-                Use the expertise and deep tech background of the best minds at AccuSoft to create a comprehensive IT strategy for a digital and technological transformation of your organization that goes in line with your business objectives. Our strategic IT consulting will help you automate and digitalise operations, optimise the software portfolio, and implement the latest technologies.
+                  Use the expertise and deep tech background of the best minds at AccuSoft to create a comprehensive IT strategy for a digital and technological transformation of your organization that goes in line with your business objectives. Our strategic IT consulting will help you automate and digitalise operations, optimise the software portfolio, and implement the latest technologies.
                 </p>
-                <div className="flex justify-center flex-wrap gap-6">
-
-                </div>
+            </div>
+            <div className="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0 z-10">
+              <img className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full" src="./imgs/pic.svg" alt="" />                                    
             </div>
           </div>
       </section>
--- src/NavBar.js
+++ src/NavBar.js
@@ -6,7 +6,7 @@ function NavBar() {
   
     return (
       <div className="container flex items-center py-4 mt-4 sm:mt-12">
-          <div className="py-1"><img src="./imgs/accusoft-logo.jpeg" width="120" height="120" alt="logo"/></div>
+          <div className="py-1"><img src="./imgs/logo.svg" width="250" height="250" alt="logo"/></div>
           <ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-accusoft-yellow uppercase text-sm" >
             <li className="cursor-pointer">Features</li>
             <li className="cursor-pointer">Pricing</li>  
