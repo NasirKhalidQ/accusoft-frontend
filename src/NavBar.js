a49f8dd8b48ed3242c1bab4ec61f2ed9751f23c7
Nasir Khalid <75773436+NasirKhalidQ@users.noreply.github.com>
2021-08-19 17:29:09 +0500

    remove chevron

--- src/NavBar.js
+++ src/NavBar.js
@@ -11,8 +11,8 @@ function NavBar() {
 					<div className="py-1"><img src="./imgs/logo.svg" width="250" height="250" alt="logo"/></div>
 					<ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-accusoft-yellow uppercase text-sm" >
 						<li className="group"> <a className="group-hover:text-accusoft-purple" href="#">Services &nbsp;</a><FontAwesomeIcon className="text-accusoft-yellow group-hover:text-accusoft-purple" icon={faChevronDown} /></li>
-						<li className="group"> <a className="group-hover:text-accusoft-purple" href="#">Pricing &nbsp;</a><FontAwesomeIcon className="text-accusoft-yellow group-hover:text-accusoft-purple" icon={faChevronDown} /></li>  
-						<li className="group"><a className="group-hover:text-accusoft-purple" href="#">Contact &nbsp;</a><FontAwesomeIcon className="text-accusoft-yellow group-hover:text-accusoft-purple" icon={faChevronDown} /></li>
+						<li> <a className="hover:text-accusoft-purple" href="#">Pricing &nbsp;</a></li>
+						<li><a className="hover:text-accusoft-purple" href="#">Contact &nbsp;</a></li>
 					</ul>
 					<div className="flex sm:hidden flex-1 justify-end">
 						<Hamburger toggled={isOpen} toggle={setOpen} color="white"/>
