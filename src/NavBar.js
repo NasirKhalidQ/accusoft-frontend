46825fc54b7541c467c6066caf258f63ee869e39
Nasir Khalid <75773436+NasirKhalidQ@users.noreply.github.com>
2021-08-19 17:41:40 +0500

    added border on hover

--- src/NavBar.js
+++ src/NavBar.js
@@ -10,9 +10,11 @@ function NavBar() {
 			<div className="container flex items-center py-4 mt-4 sm:mt-12">
 					<div className="py-1"><img src="./imgs/logo.svg" width="250" height="250" alt="logo"/></div>
 					<ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-accusoft-yellow uppercase text-sm" >
-						<li className="group"> <a className="group-hover:text-accusoft-purple" href="#">Services &nbsp;</a><FontAwesomeIcon className="text-accusoft-yellow group-hover:text-accusoft-purple" icon={faChevronDown} /></li>
-						<li> <a className="hover:text-accusoft-purple" href="#">Pricing &nbsp;</a></li>
-						<li><a className="hover:text-accusoft-purple" href="#">Contact &nbsp;</a></li>
+						<li className="group"> <a className="group-hover:text-accusoft-purple border-2 border-dashed border-accusoft-purple border-opacity-0 hover:border-opacity-100" href="#">
+							Services &nbsp;<FontAwesomeIcon className="text-accusoft-yellow group-hover:text-accusoft-purple" icon={faChevronDown} /></a>
+						</li>
+						<li> <a className="hover:text-accusoft-purple border-2 border-dashed border-accusoft-purple border-opacity-0 hover:border-opacity-100" href="#">Pricing</a></li>
+						<li><a className="hover:text-accusoft-purple border-2 border-dashed border-accusoft-purple border-opacity-0 hover:border-opacity-100" href="#">Contact</a></li>
 					</ul>
 					<div className="flex sm:hidden flex-1 justify-end">
 						<Hamburger toggled={isOpen} toggle={setOpen} color="white"/>
