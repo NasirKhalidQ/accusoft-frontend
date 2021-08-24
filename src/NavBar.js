19635e587bb3a3e4707b8511f6ebf20f80e18d50
Nasir Khalid <75773436+NasirKhalidQ@users.noreply.github.com>
2021-08-24 22:53:30 +0500

    update hamburger menu

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,48 +1,57 @@
 import Hamburger from "hamburger-react";
 import { useState } from "react";
-import { ChevronDownIcon } from "@heroicons/react/outline";
 
 function NavBar() {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <div className="container flex items-center py-4 mt-4 sm:mt-12">
-      <div className="py-1">
-        <img src="./imgs/logo.svg" width="250" height="250" alt="logo" />
+    <>
+      {isOpen && (
+        <nav className="gap-12 absolute bg-accusoft-black p-6 h-44 sm:hidden w-6/12 justify-start top-0 right-0 text-xl border-l-2 border-b-2 border-accusoft-white text-accusoft-white">
+          <ul>
+            <li className="mt-8 hover:text-accusoft-purple">Services</li>
+            <li className="mt-2 hover:text-accusoft-purple">About</li>
+            <li className="mt-2 hover:text-accusoft-purple">Contact</li>
+          </ul>
+        </nav>
+      )}
+      <div className="container flex items-center py-4 mt-4 sm:mt-12">
+        <div className="py-1">
+          <img src="./imgs/logo.svg" width="250" height="250" alt="logo" />
+        </div>
+        <ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-accusoft-yellow uppercase text-sm">
+          <li className="group">
+            {" "}
+            <a
+              className="group-hover:text-accusoft-purple border-2 border-dashed border-accusoft-purple border-opacity-0 hover:border-opacity-100 flex text-left"
+              href="#"
+            >
+              Services
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a
+              className="hover:text-accusoft-purple border-2 border-dashed border-accusoft-purple border-opacity-0 hover:border-opacity-100"
+              href="#"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              className="hover:text-accusoft-purple border-2 border-dashed border-accusoft-purple border-opacity-0 hover:border-opacity-100"
+              href="#"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+        <div className="absolute px-6 py-4 sm:hidden top-0 right-0">
+          <Hamburger toggled={isOpen} toggle={setOpen} color="white" />
+        </div>
       </div>
-      <ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-accusoft-yellow uppercase text-sm">
-        <li className="group">
-          {" "}
-          <a
-            className="group-hover:text-accusoft-purple border-2 border-dashed border-accusoft-purple border-opacity-0 hover:border-opacity-100 flex text-left"
-            href="#"
-          >
-            Services
-            <ChevronDownIcon className="text-accusoft-yellow group-hover:text-accusoft-purple w-5 h-5" />
-          </a>
-        </li>
-        <li>
-          {" "}
-          <a
-            className="hover:text-accusoft-purple border-2 border-dashed border-accusoft-purple border-opacity-0 hover:border-opacity-100"
-            href="#"
-          >
-            About
-          </a>
-        </li>
-        <li>
-          <a
-            className="hover:text-accusoft-purple border-2 border-dashed border-accusoft-purple border-opacity-0 hover:border-opacity-100"
-            href="#"
-          >
-            Contact
-          </a>
-        </li>
-      </ul>
-      <div className="flex sm:hidden flex-1 justify-end">
-        <Hamburger toggled={isOpen} toggle={setOpen} color="white" />
-      </div>
-    </div>
+    </>
   );
 }
 
